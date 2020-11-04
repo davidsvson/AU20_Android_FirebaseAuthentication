7a471fb89229d7ae813d6e1554d17ece749e409a
David Svensson <davidsvson@gmail.com>
2020-11-04 14:00:52 +0100

    fixed comment

--- app/src/main/java/se/iths/au20/firebaseauthentication/Item.kt
+++ app/src/main/java/se/iths/au20/firebaseauthentication/Item.kt
@@ -2,7 +2,7 @@ package se.iths.au20.firebaseauthentication
 
 import com.google.firebase.firestore.DocumentId
 
-data class Item(@DocumentId var documentId : String,
+data class Item(//@DocumentId var documentId : String,
                 var name: String? = null,
                 var done: Boolean = false,
                 var category: String? = null)
